8fe064e8edf48b9a09e8f05a8092a689689a34f0
chauavalois <chauavalois@hotmail.com>
2022-08-18 07:13:45 -0300

    typescript finalizado e há melhorias a serem feitas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calculadora';
+
+ @ViewChild('resultado') resultado!: ElementRef;
+
+ constructor(
+  private renderer: Renderer2
+ ){}
+
+  insert(tecla:any) : void{
+    this.resultado.nativeElement.innerHTML+=tecla;
+ }
+ clear() : void{
+  this.resultado.nativeElement.innerHTML = "";
 }
+back() : void{
+  var guardador = this.resultado.nativeElement.innerHTML;
+  this.resultado.nativeElement.innerHTML = guardador.substring(0, guardador.length -1);
+}
+calcular() : void{
+  var calculo = this.resultado.nativeElement.innerHTML;
+  if(calculo){
+    this.resultado.nativeElement.innerHTML = eval(calculo);
+  }else{
+    this.resultado.nativeElement.innerHTML = "";
+  }
+}
+
+}
+
+//var numero = document.getElementById('resultado')!.innerHTML;
+//document.getElementById('resultado')!.innerHTML = numero + num;
